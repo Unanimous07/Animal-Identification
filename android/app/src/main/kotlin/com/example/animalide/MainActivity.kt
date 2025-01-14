0e816391c1e2c11e9d4589dc56d51b9e91b52ffa
Unanimous07 <200100518@luanar.ac.mw>
2025-01-14 12:39:16 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/animalide/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animalide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? image;
+  late ImagePicker imagePicker;
+  late ImageLabeler labeler;
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+
+    ImageLabelerOptions options =
+        ImageLabelerOptions(confidenceThreshold: 0.75);
+    labeler = ImageLabeler(options: options);
+  }
+
+  chooseImage() async {
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+    if (selectedImage != null) {
+      setState(() {
+        image = File(selectedImage.path);
+        performImageClassification();
+      });
+    }
+  }
+
+  captureImage() async {
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.camera);
+    if (selectedImage != null) {
+      setState(() {
+        image = File(selectedImage.path);
+        performImageClassification();
+      });
+    }
+  }
+
+  String? result;
+  performImageClassification() async {
+    InputImage inputImage = InputImage.fromFile(image!);
+
+    final List<ImageLabel> labels = await labeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final double confidence = label.confidence;
+      result = "$text : ${confidence.toStringAsFixed(2)}\n";
+    }
+
+    setState(() {
+      result = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Center(child: Text("Animal Identification")),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(color: Colors.green, spreadRadius: 3),
+                ],
+              ),
+              child: image == null
+                  ? Icon(Icons.image_outlined, size: 400)
+                  : Image.file(image!),
+            ),
+            ElevatedButton(
+                onPressed: captureImage, child: Text("Capture Image")),
+            ElevatedButton(onPressed: chooseImage, child: Text("Choose Image")),
+            result == null ? Text(" ") : Text("Result: $result"),
+          ],
+        ),
+      ),
+    );
+  }
+}
